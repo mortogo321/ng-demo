f8c84e20c7ada1859db57b3c4df1b673d04f3834
mor <mortogo321@gmail.com>
2022-12-01 05:12:36 +0700

    init router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'employee',
+    component: EmployeeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { EmployeeComponent } from './pages/employee/employee.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, EmployeeComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, EmployeeComponent, WelcomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
